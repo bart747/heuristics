2861f9fee4ca864651cbbabc4e43025dc4717573
Bartosz Wieczorek <hello.bartosz@gmail.com>
2019-07-27 22:27:50 +0200

    remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,44 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import NoteList from './NoteList';
+import FetchAndShow from './FetchAndShow';
 import ShowSelectedOption from './ShowSelectedOption';
 
-interface AppProps {}
 
-type AppState = {
-  isCodeFirst: boolean
-}
+function App() {
 
-class App extends Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-    this.state = {
-      isCodeFirst: true,
-    };
-    this.handleClick = this.handleClick.bind(this);
+  const [isCodeFirst, setIsCodeFirst] = useState<boolean>(true);
+
+  function handleClick() {
+    setIsCodeFirst(!isCodeFirst);
   }
 
-  handleClick() {
-    this.setState(state => ({
-      isCodeFirst: !state.isCodeFirst
-    }));
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p> 
+        <section className={'selector'}>
+          <ShowSelectedOption isCodeFirst={isCodeFirst}/>
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p> 
-          <section className={'selector'}>
-            <ShowSelectedOption isCodeFirst={this.state.isCodeFirst}/>
+          <button className={'btn-round'} onClick={handleClick}>
+            <svg className={'btn-icon'} xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 24 24">
+              <path d="M19 8l-4 4h3c0 3.31-2.69 6-6 6-1.01 0-1.97-.25-2.8-.7l-1.46 1.46C8.97 19.54 10.43 20 12 20c4.42 0 8-3.58 8-8h3l-4-4zM6 12c0-3.31 2.69-6 6-6 1.01 0 1.97.25 2.8.7l1.46-1.46C15.03 4.46 13.57 4 12 4c-4.42 0-8 3.58-8 8H1l4 4 4-4H6z"/>
+              <path d="M0 0h24v24H0z"
+              fill="none"/>
+            </svg>
+          </button>    
 
-            <button className={'btn-round'} onClick={this.handleClick}>
-              <svg className={'btn-icon'} xmlns="http://www.w3.org/2000/svg"
-                width="30"
-                height="30"
-                viewBox="0 0 24 24">
-                <path d="M19 8l-4 4h3c0 3.31-2.69 6-6 6-1.01 0-1.97-.25-2.8-.7l-1.46 1.46C8.97 19.54 10.43 20 12 20c4.42 0 8-3.58 8-8h3l-4-4zM6 12c0-3.31 2.69-6 6-6 1.01 0 1.97.25 2.8.7l1.46-1.46C15.03 4.46 13.57 4 12 4c-4.42 0-8 3.58-8 8H1l4 4 4-4H6z"/>
-                <path d="M0 0h24v24H0z"
-                fill="none"/>
-              </svg>
-            </button>    
-
-          </section>
-        </header>
-        <main>
-          <NoteList isCodeFirst={this.state.isCodeFirst}/>
-        </main>
-      </div>
-      )
-  };
+        </section>
+      </header>
+      <main>
+        <FetchAndShow isCodeFirst={isCodeFirst}/>
+      </main>
+    </div>
+    )
 }
 
 export default App;
---
+++ src/FetchAndShow.tsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import NoteGroup from './NoteGroup';
+
+let notesCodeNr: number = 3;
+let notesUxNr: number = 1;
+let notesGeneralNr: number = 1;
+
+function FetchAndShow(props: any) {
+
+  const [err, setErr] = useState<any>();
+  const [notesCodeStorage, setNotesCode] = useState<string[]>([]);
+  const [notesUxStorage, setNotesUX] = useState<string[]>([]);
+  const [notesGeneralStorage, setNotesGeneral] = useState<string[]>([]);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+  function fetchNotes(category: string, notesNr: number) {
+    let newContent: any = [];
+    let quantity: number = notesNr;
+
+    for (let i = 1; i <= quantity; i++) {
+      fetch(`./content/${category}-note-${i}.json`)
+        .then(res => res.json())
+        .then(
+        (resultJson) => { 
+          let tempArr: string[] = [resultJson.content, resultJson.author]; 
+          newContent.push(tempArr); 
+
+          if (category === 'code') {
+            setNotesCode(newContent);
+          } else if (category === 'ux') {
+            setNotesUX(newContent);
+          } else {
+            setNotesGeneral(newContent);
+          } 
+          setIsLoaded(true);
+
+        },
+        (err) => {
+          setErr(err);
+          setIsLoaded(true);
+          console.log(err);
+        })
+    }
+  }
+  
+  useEffect(() => {
+    fetchNotes('code', notesCodeNr);
+    fetchNotes('ux', notesUxNr);
+    fetchNotes('general', notesGeneralNr);
+  }, []);
+ 
+
+  if (err) {
+    return <div>Error while fetching</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+  
+  if (props.isCodeFirst) {
+    return (
+      <section className="NoteList">
+        <NoteGroup collection={notesCodeStorage} category={'code'}/>
+        <NoteGroup collection={notesGeneralStorage} category={'general'}/>
+        <NoteGroup collection={notesUxStorage} category={'ux'}/>
+      </section>
+    );
+  };
+  
+  return (
+    <section className="NoteList">
+      <NoteGroup collection={notesUxStorage} category={'ux'}/>
+      <NoteGroup collection={notesGeneralStorage} category={'general'}/>
+      <NoteGroup collection={notesCodeStorage} category={'code'}/>
+    </section>
+  );
+}
+
+export default FetchAndShow;
